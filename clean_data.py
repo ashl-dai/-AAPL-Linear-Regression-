3d50fa515c30a344624cbb2f14f91cf04fd8179d
Benjamin Yu <byu@Theo.local>
2026-03-10 16:10:17 -0700

    Dataset

--- clean_data.py
+++ clean_data.py
@@ -1,1 +1,27 @@
-import pandas as pd 
+import yfinance as yf
+import plotly.express as px
+
+aapl = yf.Ticker('AAPL')
+df = aapl.history(period = '10y')
+
+missing = df.isnull().sum()
+print(missing)
+
+nan = df.isna().sum()
+print(nan)
+
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+outliers = df[(df < lower_bound) | (df > upper_bound)]
+print(outliers)
+
+fig = px.scatter(df, y = "Close", title = 'Closing Price of AAPL Over the Past 10 Years')
+fig.show()
+
+csvFile = 'AAPL_Close_Price.csv'
+df['Close'].to_csv(csvFile, index = False)
